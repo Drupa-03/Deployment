903ddc622b91ed8f5e511384dce83fa221c4b723
Drupa03 <drupakhatri@gmail.com>
2025-05-02 12:16:59 +0530

    changes

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
 
-export const authOptions={
-    providers: [
-        GithubProvider({
-            clientId:'Ov23li0cKePt6S7Hsigf',
-            clientSecret:'dd69d86e608cddd477b9ca56dacd391283655d32'
-        }),
-    ],
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
